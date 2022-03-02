aaf852295932373a2764e5db538d22916df69114
dongnguyen248 <dacdongdhv@gmail.com>
2022-03-02 13:41:45 +0700

    update code

--- src/components/modals/AddQuantity.jsx
+++ src/components/modals/AddQuantity.jsx
@@ -1,275 +1,203 @@
-import { useState } from 'react';
-import DatePicker from 'react-datepicker';
-import './InventoryModals.css';
-function AddQuantity() {
+import { useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "./InventoryModals.css";
+function AddQuantity({ data }) {
+    
     const handleAddQuantity = () => {
-        console.log('first');
+        console.log("first");
     };
     const [quantity, setQuantity] = useState({
-        qCode: '',
         inputDate: new Date(),
-        quantity: '',
-        price: '',
-        poNumber: '',
-        supplier: '',
-        buyer: '',
-        requester: '',
+        quantity: "",
+        price: "",
+        poNumber: "",
+        supplier: "",
+        buyer: "",
+        requester: "",
         received: false,
-        locator: '',
+        locator: "",
         checked: false,
-        datecheck: '',
-        checker: '',
-        checkResult: '',
+        datecheck: new Date(),
+        checker: "",
+        checkResult: "",
     });
+    
     return (
         <>
-            <table className='table table-bordered'>
+            <table className="table table-bordered">
                 <tbody>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>QCode</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        qCode: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].qcode}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Input Date</label>
                         </td>
                         <td>
                             <DatePicker
-                                className='form-control'
+                                className="form-control"
                                 selected={quantity.inputDate}
-                                onChange={(date: Date) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        checkedDate: date,
-                                    })
-                                }
+                                defaultValue={(quantity.inputDate = new Date())}
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Quantity</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        quantity: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].quantity}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Price</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        price: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].price}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>PO Number</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        poNumber: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].poNumber}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Supplier</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        supplier: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].supplier}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Buyer</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        buyer: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].buyer}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Requester</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        requester: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].requester}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Received</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        received: e.target.checked,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={(data[0].received = true)}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Locator</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        locator: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].locator}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Checked</label>
                         </td>
                         <td>
-                            <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        checked: e.target.checked,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
-                            />
+                            <input type="text" className="form-control" />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Date Check</label>
                         </td>
                         <td>
                             <DatePicker
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        datecheck: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={(quantity.datecheck = new Date())}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Checker</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        checker: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={data[0].checker}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                     <tr>
-                        <td className='tdleft'>
+                        <td className="tdleft">
                             <label>Check Result</label>
                         </td>
                         <td>
                             <input
-                                onChange={(e) =>
-                                    setQuantity({
-                                        ...quantity,
-                                        checkResult: e.target.value,
-                                    })
-                                }
-                                type='text'
-                                className='form-control'
+                                defaultValue={(data[0].checkResult = "Accept")}
+                                type="text"
+                                className="form-control"
                             />
                         </td>
                     </tr>
                 </tbody>
             </table>
-            <div className='col-md-12 text-center'>
+            <div className="col-md-12 text-center">
                 <button
-                    type='button'
-                    className='btn btn-primary me-3'
-                    onClick={handleAddQuantity}>
+                    type="button"
+                    className="btn btn-primary me-3"
+                    onClick={handleAddQuantity}
+                >
                     Save Material
                 </button>
             </div>
--- src/pages/Inventory/Inverntory.jsx
+++ src/pages/Inventory/Inverntory.jsx
@@ -1,13 +1,13 @@
-import DataTable from 'react-data-table-component';
-import Header from '../../components/header/Header';
-import Checkbox from '@material-ui/core/Checkbox';
-import { Modal } from 'react-bootstrap';
-import { useState } from 'react';
-import NewMaterial from 'components/modals/NewMaterial';
-import AddQuantity from 'components/modals/AddQuantity';
-import InventorySearch from 'components/searchingForm/InventorySearch';
-import './Inventory.css';
-import inventories from './datainventory';
+import DataTable from "react-data-table-component";
+import Header from "../../components/header/Header";
+import { Modal } from "react-bootstrap";
+import swal from 'sweetalert';
+import { useState, useEffect, useCallback } from "react";
+import NewMaterial from "components/modals/NewMaterial";
+import AddQuantity from "components/modals/AddQuantity";
+import InventorySearch from "components/searchingForm/InventorySearch";
+import "./Inventory.css";
+import inventories from "./datainventory";
 
 export default function Inverntory() {
     const [newMaterial, setNewMaterial] = useState(false);
@@ -17,59 +17,82 @@ export default function Inverntory() {
 
     const columns = [
         {
-            name: 'Zone',
+            name: "Zone",
             selector: (row) => row.zone,
+            grow: 0.5,
+            wrap: true,
         },
         {
-            name: 'Qcode',
+            name: "Qcode",
             selector: (row) => row.qcode,
+            grow: 0.5,
         },
         {
-            name: 'Item',
+            name: "Item",
             selector: (row) => row.item,
+            wrap: true,
         },
         {
-            name: 'Location',
+            name: "Location",
             selector: (row) => row.location,
+            grow: 0.5,
         },
         {
-            name: 'Specification',
+            name: "Specification",
             selector: (row) => row.spec,
+            wrap: true,
         },
         {
-            name: 'Unit',
+            name: "Unit",
+            grow: 0.3,
             selector: (row) => row.unit,
         },
         {
-            name: 'Quantity',
+            name: "Quantity",
             selector: (row) => row.quantity,
+            grow: 0.3,
         },
         {
-            name: 'Remark',
-            selector: (row) => row.remark,
-        },
-        {
-            name: 'Locator',
+            name: "Locator",
             selector: (row) => row.locator,
         },
+        {
+            name: "Remark",
+            selector: (row) => row.remark,
+        },
+
+        {
+            cell: (row) => (
+                <button
+                    className="btn btn-primary btn-sm"
+                    onClick={handleEdit(row)}
+                >
+                    Edit
+                </button>
+            ),
+            grow: 0.3,
+            // cell: row => <button onClick={handleEdit(row)}>Edit</button>
+        },
     ];
+    const handleEdit = useCallback((row) => async () => {
+        console.log(row);
+    });
     const selectedRowStyle = [
         {
             when: (row) => row.toggleSelected,
             style: {
-                backgroundColor: '#dff0d8',
-                userSelect: 'none',
+                backgroundColor: "#dff0d8",
+                userSelect: "none",
             },
         },
     ];
 
     const handleRowClicked = (row) => {
-        console.log(Checkbox);
         const updatedData = dataInventories.map((item) => {
             if (row.id !== item.id) {
+                delete item.toggleSelected;
                 return item;
             }
-
             return {
                 ...item,
                 toggleSelected: !item.toggleSelected,
@@ -78,29 +101,47 @@ export default function Inverntory() {
 
         setDataInventories(updatedData);
     };
-
+    const itemSelected = dataInventories.filter(
+        (item) => item.toggleSelected === true
+    );
+    const handleAddQuantity = () => {
+        !itemSelected.length ? swal("Please select an  item!") : setAddQuantity(true);;
+    };
+    const ExpandedComponent = (row) => {
+        let data = row.data.qcode;
+        return <>{row.data.qcode}</>;
+    };    
     return (
         <>
             <Header />
-            <div className='swrap'>
-                <h1 className='header__list'>Inventory List</h1>
-                <div className='me-2 d-flex float-end'>
+            <div className="swrap">
+                <h1 className="header__list">Inventory List</h1>
+                <div className="me-2 d-flex float-end">
                     <button
-                        type='button'
-                        className='btn btn-primary button__export'
-                        onClick={() => setNewMaterial(true)}>
+                        type="button"
+                        className="btn btn-primary btn-sm button__export"
+                    >
+                        Export Excel
+                    </button>
+                    <button
+                        type="button"
+                        className="btn btn-primary btn-sm button__export"
+                        onClick={() => setNewMaterial(true)}
+                    >
                         New Material
                     </button>
                     <button
-                        type='button'
-                        className='btn btn-primary button__export'
-                        onClick={() => setAddQuantity(true)}>
+                        type="button"
+                        className="btn btn-primary btn-sm button__export"
+                        onClick={handleAddQuantity}
+                    >
                         Add Quantity
                     </button>
                     <button
-                        type='button'
-                        className='btn btn-primary button__export'
-                        onClick={() => setExportMaterial(true)}>
+                        type="button"
+                        className="btn btn-primary btn-sm button__export"
+                        onClick={() => setExportMaterial(true)}
+                    >
                         Export Material
                     </button>
                 </div>
@@ -108,21 +149,22 @@ export default function Inverntory() {
                 <DataTable
                     columns={columns}
                     data={dataInventories}
-                    selectableRowsSingle
-                    selectableRows
-                    selectableRowsComponent={Checkbox}
+                    expandableRows
                     onRowClicked={handleRowClicked}
+                    expandableRowExpanded={(row) => row.defaultExpanded}
                     conditionalRowStyles={selectedRowStyle}
+                    expandableRowsComponent={ExpandedComponent}
                     pagination
                 />
             </div>
             <Modal
-                size='lg'
+                size="lg"
                 show={newMaterial}
                 onHide={() => setNewMaterial(false)}
-                aria-labelledby='example-modal-sizes-title-lg'>
+                aria-labelledby="new-modal-sizes-title-lg"
+            >
                 <Modal.Header closeButton>
-                    <Modal.Title id='example-modal-sizes-title-lg'>
+                    <Modal.Title id="new-modal-sizes-title-lg">
                         New Material
                     </Modal.Title>
                 </Modal.Header>
@@ -131,26 +173,28 @@ export default function Inverntory() {
                 </Modal.Body>
             </Modal>
             <Modal
-                size='lg'
+                size="lg"
                 show={addQuantity}
                 onHide={() => setAddQuantity(false)}
-                aria-labelledby='example-modal-sizes-title-lg'>
+                aria-labelledby="add-modal-sizes-title-lg"
+            >
                 <Modal.Header closeButton>
-                    <Modal.Title id='example-modal-sizes-title-lg'>
+                    <Modal.Title id="add-modal-sizes-title-lg">
                         Add Quantity
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <AddQuantity />
+                    <AddQuantity data={itemSelected} />
                 </Modal.Body>
             </Modal>
             <Modal
-                size='lg'
+                size="lg"
                 show={exportMaterial}
                 onHide={() => setExportMaterial(false)}
-                aria-labelledby='example-modal-sizes-title-lg'>
+                aria-labelledby="export-modal-sizes-title-lg"
+            >
                 <Modal.Header closeButton>
-                    <Modal.Title id='example-modal-sizes-title-lg'>
+                    <Modal.Title id="export-modal-sizes-title-lg">
                         Export Material
                     </Modal.Title>
                 </Modal.Header>
