f92e299917dc438b2dc6bbce383cd10787b81160
dongnguyen248 <dacdongdhv@gmail.com>
2022-03-08 16:41:40 +0700

    fix inventories store

--- src/services/inventoriesService.js
+++ src/services/inventoriesService.js
@@ -1,26 +1,30 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import { publicRequest, userRequest } from "utilities/requestMethod";
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { publicRequest, userRequest } from 'utilities/requestMethod';
+
 export const getInventories = createAsyncThunk(
-    "getInventories",
-    async (data,dispatch, getState) => {
-        console.log(data)
-        const res = await publicRequest.get(`/material/${data.currentPage}/${data.perPage}`);
+    'getInventories',
+    async (data, dispatch, getState) => {
+        console.log(data);
+        const res = await publicRequest.get(
+            `/material/${data.currentPage}/${data.perPage}`,
+        );
         return res.data;
-    }
+    },
 );
 export const addInventories = createAsyncThunk(
-    "addInventories",
-    async(data,{ rejectWithValue })=>{
-        console.log(data)
-        const res = await userRequest.post("/addinventories",data);
+    'addInventories',
+    async (data, { rejectWithValue }) => {
+        console.log(data);
+        const res = await userRequest.post('/addinventories', data);
         return res.data;
-    }
-    )
+    },
+);
 export const searchInventories = createAsyncThunk(
-    "searchInventories",
-    async(data,{rejectWithValue})=>{
-        console.log(data)
-        const res = await publicRequest.get(`material/${data.currentPage}/${data.perPage}/search?qcode=${data.qCode}&zone=${data.area}&location=${data.location}&item=${data.item}&specification=${data.spec}`)
-        return res.data
-    }
-)
+    'searchInventories',
+    async (data, { rejectWithValue }) => {
+        const res = await publicRequest.get(
+            `material/${data.currentPage}/${data.perPage}/search?qcode=${data.qCode}&zone=${data.area}&location=${data.location}&item=${data.item}&specification=${data.spec}`,
+        );
+        return res.data;
+    },
+);
